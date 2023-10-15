27c5f192411a49698738ceb9b5798325c28a2345
akik-hossain <akikhs00@gmail.com>
2023-10-15 17:39:54 +0600

    installed environemnt for this project

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-center text-5xl font-bold">
+        Hello this is Home component
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
